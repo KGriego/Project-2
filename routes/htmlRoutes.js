3506bd17d19c3270e079e15dd29f41e31a12b182
DutchMon <dutch.holland13@gmail.com>
2018-08-16 13:29:49 -0700

    i really hate you

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -21,15 +21,8 @@ module.exports = function(app) {
   app.get("/signUp", function(req, res) {
     res.render("signUp");
   });
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/payment", function(req, res) {
+    res.render("payment");
   });
 
   // Render 404 page for any unmatched routes
