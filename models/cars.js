1e5d0ff37d2a280a5449a11d71039f68d4ab269f
DutchMon <dutch.holland13@gmail.com>
2018-08-16 14:22:27 -0700

    final merge

--- models/cars.js
+++ models/cars.js
@@ -1,17 +1,38 @@
-// for accessing database
-
-var db = require("./");
-
-module.exports = function (acriss) {
- db.cars.findAll({
-        where: {
-            acriss_code: acriss
-        }
-    }).then(function (data) {
-        return data;
-    })
+anuhya [2:22 PM]
+cars.js 
+module.exports = function(sequelize, DataTypes) {
+var cars = sequelize.define("cars", {
+ id: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  autoIncrement: true,
+  primaryKey: true
+ },
+ model: {
+  type: DataTypes.STRING,
+  allowNull: false
+ },
+ transmission: {
+  type: DataTypes.TINYINT,
+  allowNull: false
+ },
+ trunkSpace: {
+  type: DataTypes.TINYINT,
+  allowNull: false
+ },
+ people: {
+  type: DataTypes.TINYINT,
+  allowNull: false
+ },
+ imgSRC: {
+  type: DataTypes.STRING,
+  allowNull: false
+ },
+ company: {
+  type: DataTypes.STRING,
+  allowNull: false
+ }
+​
+});
+ return cars;
 };
-
-
-
-
