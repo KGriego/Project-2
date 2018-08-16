7c7879ed1ff55f2f6633e6d94ce51e092c432bea
KevGri98 <kev.gri32@gmail.com>
2018-08-15 21:57:54 -0700

    changed routes, added user login/create user, added new packages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,36 +1,28 @@
 var db = require("../models");
 
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    res.render("index");
-  });
-
-  app.get("/login", function(req, res) {
-    res.render("login");
-  });
-
-  app.get("/findCar", function(req, res) {
-    res.render("findCar");
-  });
-
-  app.get("/locations", function(req, res) {
-    res.render("locations");
-  });
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    } else {
+      res.render("login", {layout: "login"});
+    }
+  })
 
   app.get("/signUp", function(req, res) {
-    res.render("signUp", {layout: "signup"});
+    res.render("signUp", {layout: "signup"})
+  })
+
+  app.get("/home", isAuthenticated, function(req, res) {
+    res.render("index", {layout: "index"});
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/index", function(req, res) {
+    res.render("index");
   });
 
   // Render 404 page for any unmatched routes
